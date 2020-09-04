a2cf2b9a3abc609d367d59bf6e4626495a3cf70c
Roel Antonisse <roel.antonisse@gmail.com>
2020-09-04 10:11:36 +0200

    Add a language-preview.svelte file
    (testing purposes)

---
+++ language-preview.svelte
@@ -0,0 +1,39 @@
+<svelte:head>
+  <title>Language Svelte</title>
+</svelte:head>
+
+{#if paragraph}
+  <p class="">{@html paragraph}</p>
+{:else if Svelte}
+  <p>There is no para&shy;graph...But here is, ...</p>
+  <Svelte>!
+{/if}
+
+{#each items as item}
+  <svelte:component this={item}>
+{:else if item == Svelte}
+  <Svelte on:click>
+{/each}
+
+<slot></slot>
+
+<style>
+  p {
+    margin-bottom: 3em;
+  }
+</style>
+
+<script>
+  import Svelte from './Svelte.svelte';
+
+  export let paragraph = 'Hello Svelte';
+</script>
+
+<script context="module">
+  import Svelte from './Svelte.svelte';
+
+  export let paragraph = 'Hello Svelte';
+  export let items = [];
+
+  $: withName = `Rvantonisse says: ${paragraph}`;
+</script>
